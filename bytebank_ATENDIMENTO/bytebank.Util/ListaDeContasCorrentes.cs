3f1406e64f3186f4a51cf1016dcaa66da7fd87a4
Lucas Pinheiro <lucaslogos@gmail.com>
2023-12-12 16:27:27 -0300

    update

---
+++ bytebank_ATENDIMENTO/bytebank.Util/ListaDeContasCorrentes.cs
@@ -0,0 +1,18 @@
+using bytebank.Modelos.Conta;
+
+namespace bytebank_ATENDIMENTO.bytebank.Util;
+
+public class ListaDeContasCorrentes
+{
+    private ContaCorrente[] itens = null;
+    private int _proximaPosicao = 0;
+    public ListaDeContasCorrentes(int tamanhoInicial = 5)
+    {
+        itens = new ContaCorrente[tamanhoInicial];
+    }
+    public void Adicionar(ContaCorrente item)
+    {
+        itens[_proximaPosicao] = item;
+        _proximaPosicao++;
+    }
+}
